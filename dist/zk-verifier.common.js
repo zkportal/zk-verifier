79f764c06f878a2bc6b00d42897c45d504fb936b
Lev Sizov <lev.sizov@summitto.com>
2023-04-14 17:29:27 +0200

    Release 2.0.0

--- dist/zk-verifier.common.js
+++ dist/zk-verifier.common.js
@@ -1,5 +1,5 @@
 /*
-* zk-verifier v2.0.0-rc.0 (https://github.com/zkportal/zk-verifier)
+* zk-verifier v2.0.0 (https://github.com/zkportal/zk-verifier)
 * Based on: ark-circom (https://github.com/gakonst/ark-circom)
 * Copyright 2021 Georgios Konstantopoulos (https://github.com/gakonst)
 * Copyright 2022-2023 zkPortal (https://github.com/zkportal)
